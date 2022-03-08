767f7c8b1cf238044b2d4c5f1a2985ab959bfc78
jelipo <me@jelipo.com>
2022-03-09 01:00:18 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,20 @@ use unicorn_engine::{RegisterARM, SECOND_SCALE, Unicorn};
 use unicorn_engine::unicorn_const::{Arch, Mode, Permission};
 
 fn main() {
-    let test1 = test("dasda".to_string());
-
-
-    let arm_code32: Vec<u8> = vec![0x17, 0x00, 0x40, 0xe2]; // sub r0, #23
+    let arm_code32 = 0b0000_0001_0010_1111_1111_1111_0001_0010u32.to_le_bytes();
 
     let mut unicorn = Unicorn::new(Arch::ARM, Mode::LITTLE_ENDIAN).expect("failed to initialize Unicorn instance");
-    let emu = unicorn.borrow_mut();
+    let emu = &mut unicorn;
     emu.mem_map(0x1000, 0x4000, Permission::ALL).expect("failed to map code page");
-    emu.mem_write(0x1000, &arm_code32).expect("failed to write instructions");
 
-    emu.reg_write(RegisterARM::R0, 123).expect("failed write R0");
-    emu.reg_write(RegisterARM::R5, 1337).expect("failed write R5");
+    emu.mem_write(0x1000, &arm_code32).unwrap();
 
-    let _ = emu.emu_start(0x1000, (0x1000 + arm_code32.len()) as u64, 10 * SECOND_SCALE, 1000);
-    assert_eq!(emu.reg_read(RegisterARM::R0), Ok(100));
-    assert_eq!(emu.reg_read(RegisterARM::R5), Ok(1337));
+    emu.reg_write(RegisterARM::R2, 0x1100).unwrap();
+
+    let _ = emu.emu_start(0x1000, (0x1000 + arm_code32.len()) as u64, 10 * SECOND_SCALE, 1);
+
+
+    let i = emu.reg_read(RegisterARM::R15).unwrap();
+    println!("{:x}", i);
 }
 
-fn test<D: AsRef<[u8]>>(data: D) {
-    let string = String::new();
-    string.borrow()
-    let data_ref = data.borrow();
-    Borrow::borrow()
-    let x1 = data_ref[0];
-}
